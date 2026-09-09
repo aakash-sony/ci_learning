54db3f6973357700fef940dd46c35a705dcee15e
akash <aakashsoni8756@gmail.com>
2026-09-09 14:08:27 +0530

    changes 2222

--- learning/src/main/java/com/ci/learning/LearningApplication.java
+++ learning/src/main/java/com/ci/learning/LearningApplication.java
@@ -9,6 +9,7 @@ public class LearningApplication {
 	public static void main(String[] args) {
 		System.out.println("main class started");
 		System.out.println("changes for CI pipeline checking");
+		System.out.println("changes for CI last checking 2");
 		SpringApplication.run(LearningApplication.class, args);
 	}
 
