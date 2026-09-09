6555d8d331cdf1a2fb8dc5ec2a3daab75742af03
akash <aakashsoni8756@gmail.com>
2026-09-09 13:41:43 +0530

    file updated

--- learning/src/main/java/com/ci/learning/LearningApplication.java
+++ learning/src/main/java/com/ci/learning/LearningApplication.java
@@ -9,8 +9,6 @@ public class LearningApplication {
 	public static void main(String[] args) {
 		System.out.println("main class started");
 		System.out.println("changes for CI pipeline checking");
-		System.out.println("changes for fwjh checking");
-
 		SpringApplication.run(LearningApplication.class, args);
 	}
 
